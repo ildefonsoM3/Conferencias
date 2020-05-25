bb32b9049d5330fccdc9cdde545e261de6a8e1b6
ildefonso <ildefonso.muro@gmail.com>
2020-05-24 23:19:43 -0500

    Pantalla de inicio, creados todos los activities de la aplicación

---
+++ app/src/main/java/com/im3/conf/ui/activities/splashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.im3.conf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.im3.conf.R
+
+class splashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
