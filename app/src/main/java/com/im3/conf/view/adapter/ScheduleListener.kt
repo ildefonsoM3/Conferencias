1a0592a9c3d665c61a94f624dbc2e8444b1a8f29
Ildefonso Muro <ildefonso.muro@gmail.com>
2020-06-22 00:03:44 -0500

    Obtencion de datos del schedule fragment. Fix

--- app/src/main/java/com/im3/conf/view/adapter/ScheduleListener.kt
+++ app/src/main/java/com/im3/conf/view/adapter/ScheduleListener.kt
@@ -1,5 +1,9 @@
 package com.im3.conf.view.adapter
 
+import android.os.Bundle
+import com.im3.conf.model.Conference
+
 interface ScheduleListener {
     fun onConferenceClicked(conference: com.im3.conf.model.Conference, position: Int)
+    abstract fun bundleOf(pair: Pair<String, Conference>): Bundle?
 }
